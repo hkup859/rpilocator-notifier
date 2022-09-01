bb0343d514e4e7394cc5aa08cf7ae4f54061d1dc
Davis Family <20144454+hkup859@users.noreply.github.com>
2022-09-01 12:31:36 -0400

    status update change

--- src/cron.js
+++ src/cron.js
@@ -1,6 +1,7 @@
 const cron = require('node-cron')
 const puppeteer = require('puppeteer')
 const dotenv = require('dotenv')
+// const fs = require('fs')
 
 // Allow pulling ENV variable from .env file
 dotenv.config()
@@ -10,6 +11,7 @@ const authToken = process.env.TWILIO_AUTH_TOKEN || ''
 const fromPhoneNumber = process.env.TWILIO_PHONE_NUMBER || ''
 const toPhoneNumber = process.env.PERSONAL_PHONE_NUMBER || ''
 const client = require('twilio')(accountSid, authToken)
+let stockData = []
 
 
 cron.schedule('* 6-23 * * *', async function() {
@@ -44,16 +46,34 @@ cron.schedule('* 6-23 * * *', async function() {
 const checkStock = async (browser) => {
     console.log("In checkStock Function")
     try {
+      let newInStockItemFound = false
       let page = await browser.newPage();
       await page.setViewport({width: 1920, height: 1080})
       await page.goto('https://rpilocator.com/?country=US')
       await page.waitForSelector('td[class="sorting_1"]', {timeout: 30000}).catch((err) => console.log(`STEP 1: ${err.message}`))
       const stockOptions = await page.$$('td[class="sorting_1"]')
+      const productLinks = await page.$$('td a')
       for(let i = 0; i < stockOptions.length; i++) {
         const text = await getTextContent(stockOptions[i])
-        if (text === 'Yes') return true
+        const outerHTMLElement = await productLinks[i*2].getProperty('outerHTML')
+        const outerHTMLText = await (outerHTMLElement).jsonValue()
+        const currentStockStatus = text === 'Yes'
+        if (stockData.length === stockOptions.length) {
+          const lastStockIndex = stockData.findIndex(x => x.item === outerHTMLText)
+          const lastStockInfo = stockData[lastStockIndex]
+          if (!lastStockInfo.stockStatus && currentStockStatus) {
+            console.log("Product now in stock!")
+            newInStockItemFound = true
+          }
+          lastStockInfo.stockStatus = currentStockStatus
+        } else {
+          stockData.push({
+            item: outerHTMLText,
+            stockStatus: currentStockStatus
+          })
+        } 
       }
-      return false
+      return newInStockItemFound
     } catch (err) {
       console.log(`checkStock Function Failed: ${err}`)
       throw err
